ce2bcb5a46db6b9c34fcf496176777025d67f582
Andy Bettisworth <andy@accreu.com>
2017-11-19 18:15:25 -0600

    return a timestamp of length 14

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+let now = new Date()
+let year = now.getFullYear()
+let month = now.getMonth() + 1
+let day = now.getDate()
+let hours = now.getHours()
+let minutes = now.getMinutes()
+let seconds = now.getSeconds()
+
+let timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`
+
+/**
+ * Exports timestamp
+ */
+
+module.exports = timestamp
--- test.js
+++ test.js
@@ -15,4 +15,7 @@ describe("g_migration", () => {
   it("should have tests", () => {
     assert.ok(true)
   })
+  it("should return a timestamp of length 14", () => {
+    assert.equal(g_migration.length, 14)
+  })
 })
